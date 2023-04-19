2ec0567383f3eda9fdc80e196f04475ed08e71c3
nivedita <nivedita.singh.bca2020@bbdu.ac.in>
2023-04-19 13:27:14 +0530

    base

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('login.html')
-
+def login():
+    return render_template('login.html',title="login/register")
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8000, debug=True)
- 
+  app.run(host='0.0.0.0', port=8000, debug= True)
+
+
