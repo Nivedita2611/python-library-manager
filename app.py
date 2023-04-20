c202c5444ca9c9e531ac08c3fae243a6fb93c90f
prashansha14 <singhthakurprashansha@gmail.com>
2023-04-20 14:17:31 +0530

    done

--- app.py
+++ app.py
@@ -4,8 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def login():
-    return render_template('login.html',title="login/register")
+    return render_template('login.html',title="login/registe")
 
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/library')
+def library_list():
+    return render_template('list.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8000, debug= True)
