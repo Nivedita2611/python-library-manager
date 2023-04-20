c7a731aec416ed9105dd8bfe8736f3ab9a9c4c9a
nivedita <nivedita.singh.bca2020@bbdu.ac.in>
2023-04-20 14:57:48 +0530

    auth system done

--- app.py
+++ app.py
@@ -1,22 +1,97 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, flash, url_for
+from sqlalchemy import create_engine
+from sqlalchemy import engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+from database import User
+
+import re
+
 app = Flask(__name__)
+app.secret_key = "the basics of life with python"
 
+# for validating an Email 
+def validate_email(email):  
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    if(re.search(regex,email)):  
+        return True 
+    return False
 
-@app.route('/')
-def login():
-    return render_template('login.html',title="login/registe")
+def get_db():
+    engine = create_engine('sqlite:///database.sqlite')
+    Session = scoped_session(sessionmaker(bind=engine))
+    return Session()
+    
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username:
+            if password and len(password)>=6:
+                try:
+                    sess = get_db()
+                    user = sess.query(User).filter_by(name=username,password=password).first()
+                    if user:
+                        session['isauth'] = True
+                        session['email'] = user.email
+                        session['id'] = user.id
+                        session['name'] = user.name
+                        del sess
+                        flash('login successfull','success')
+                        return redirect('/home')
+                    else:
+                        flash('email or password is wrong','danger')
+                except Exception as e:
+                    flash(e,'danger')
+    return render_template('login.html',title='login')
+
+@app.route('/signup',methods=['GET','POST'])
 def signup():
-    return render_template('signup.html',title="signup")
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        cpassword = request.form.get('cpassword')
+        if name and len(name) >= 3:
+            if email and validate_email(email):
+                if password and len(password)>=6:
+                    if cpassword and cpassword == password:
+                        try:
+                            sess = get_db()
+                            newuser = User(name=name,email=email,password=password)
+                            sess.add(newuser)
+                            sess.commit()
+                            del sess
+                            flash('registration successful','success')
+                            return redirect('/')
+                        except:
+                            flash('email account already exists','danger')
+                    else:
+                        flash('confirm password does not match','danger')
+                else:
+                    flash('password must be of 6 or more characters','danger')
+            else:
+                flash('invalid email','danger')
+        else:
+            flash('invalid name, must be 3 or more characters','danger')
+    return render_template('signup.html',title='register')
 
-@app.route('/')
-def search():
-    return render_template('search',title="search")
+@app.route('/home',methods=['GET','POST'])
+def home():
+    if session.get('isauth'):
+        username = session.get('name')
+        return render_template('home.html',title=f'Home|{username}')
+    flash('please login to continue','warning')
+    return redirect('/')
 
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
+@app.route('/logout')
+def logout():
+    if session.get('isauth'):
+        session.clear()
+        flash('you have been logged out','warning')
+    return redirect('/')
 
 @app.route('/library')
 def library_list():
